2e0c4f0e15ccc27a15b8c13f5b99cb32b60203a1
Damian Aquino <118084592+Efedeequis85@users.noreply.github.com>
2024-11-15 00:51:34 -0300

    Update clienteTCP.py

--- sockets/clienteTCP.py
+++ sockets/clienteTCP.py
@@ -1,24 +1,14 @@
-# Con el cliente podemos conectarnos a un servidor TCP que este a al escucha
-
 import socket
 
-# AF_INET para indicar que sera un socket IPV4
-# SOCK_STREAM para indicar que sera TCP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# El metodo conect recibe una tupla donde deben estar los datos del servidor
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 12345)
-sock.connect(server_address)
+server.connect(server_address)
+solicitud = "SOLUCITUD"
 
 try:
-  # Enviar datos al servidor
-  message = "Hola servidor!"
-  client_socket.sendall(message.encode())
-
-  # El metodo recv bloquea la ejecucion del programa hasta que se reciban datos del servidor
+  server.sendall(message.encode())
   data = client_socket.recv(1024)
+  
   print(f"Datos recibidos: {data.decode()}")
-
 finally:
-  # Por ultimo siempre debemos cerrar la conexions
   sock.close()
