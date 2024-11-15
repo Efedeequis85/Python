ebcc30ac894efe3916462cecbd13f1ec3b21aa1d
Damian Aquino <118084592+Efedeequis85@users.noreply.github.com>
2024-11-15 00:49:32 -0300

    Update servidorTCP.py

--- sockets/servidorTCP.py
+++ sockets/servidorTCP.py
@@ -1,33 +1,22 @@
-# Los sockets se usan para enviar datos a traves de la red de un equipo a otro.
 import socket
 
-# AF_INET para indicar que sera un socket IPV4
-# SOCK_STREAM para indicar que sera TCP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Interefaz y puerto donde recibira los datos
-server_address = ('localhost', 12345)
+server_address = ('127.0.0.1', 60000)
 server_socket.bind(server_address)
-
-# Podemos indicarle el numero maximo de conexiones que queremos que reciba
 server_socket.listen(1)
 
 while True:
-  # El metodo accept bloquea la ejecucion del programa hasta que se reciba una conexion. Devuelve el socket con el que interactuamos con el cliente y su respectiva direccion
   clienteSocket, clienteDir = server_socket.accept()
-
   try:
     while True:
     data = clienteSocket.recv(1024)
 
-    # Que no se hallan recibido datos puede ser señal de que se cerro la conexion
+
       if not data:
         break
+        
       print(f"Datos: {data.decode()}")
-
-      # Devolver al cliente los datos de repuesta
       client_socket.sendall(b"Datos recibidos: " + data)
-
   finally:
-    # Para finalizar siempre sebemos cerrar el socker    
+  
     clienteSocket.close()
